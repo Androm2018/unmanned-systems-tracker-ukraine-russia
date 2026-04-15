c664fecdb8fc82ffda9a000e9a76a83ba68ea2c3
Androm2018 <47905905+Androm2018@users.noreply.github.com>
2026-04-15 10:06:42 +0100

    Delete tracker-new-files/config.js

--- tracker-new-files/config.js
+++
@@ -1,3 +0,0 @@
-// Unmanned Systems Tracker — shared config
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzWLX6DF8VxV41OQt4rUyCvhaHH0dimr2LBRgys89TWiouYEhvlDVcACwQMbU3uaSWGCA/exec';
-const TRACKER_VERSION = '2026.04';
